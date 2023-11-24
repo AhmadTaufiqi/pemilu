a866730520912f3fadb2fb05c335eab7f88823e9
ataufiqi <ahmadtaufiky@gmail.com>
2023-11-24 15:28:25 +0700

    save relwaan

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -47,10 +47,39 @@ class User extends CI_Controller
         $data['user'] = $this->db->get_where('user', ['username' => $this->session->userdata('username')])->row_array();
         $data['title'] = "Tambah Data Relawan";
 
+        if($this->input->post()){
+            $input = $this->input->post();
+            $data_save = array(
+                'nik' => $input['nik'],
+                'nama' => $input['nama'],
+                'gender' => $input['gender'],
+                'propinsi' => $input['propinsi'],
+                'kabupaten' => $input['kabupaten'],
+                'kota' => $input['kota'],
+                'kecamatan' => $input['kecamatan'],
+                'kelurahan' => $input['kelurahan'],
+                'rw' => $input['rw'],
+                'rw' => $input['rw'],
+                'tps' => $input['tps'],
+                'inputter' => $input['inputter'],
+                'created_at' => now()
+            );
+            if($input['relawan_id']){
+                $this->db->update('relawan',['relawan_id' => $input['relawan_id']]);
+            }else{
+                $this->db->insert('relawan',$data);
+            }
+
+        }
+
         $this->load->view('templates/header', $data);
         $this->load->view('templates/topbar', $data);
         $this->load->view('templates/menubar', $data);
         $this->load->view('user/add-relawan', $data);
         $this->load->view('templates/footer', $data);
+    }
+
+    public function ExportRelawan(){
+
     }
 }
